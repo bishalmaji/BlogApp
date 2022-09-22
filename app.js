88d8df16192d4b0cea165a519873e9ef2dcc2f6a
bishalmaji <bishalmaji60.lj@gmail.com>
2022-09-22 13:49:24 +0530

    My first commit

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ import mongoose from 'mongoose';
 import blogRouter from './routes/blog-routes';
 import router from './routes/user-routes';
 
-// mongo password=lyQ2Vf0zv7XwOuam
-
-
+require('dotenv').config({ path: 'DATABASE' });
 const app=express();
 app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
+
 app.get('/',(req,res)=>{
     res.send('<h1>Welcome to Blog App API</h1>M');
     res.end();
-})
+});
 mongoose.connect(process.env.DATABASE
 )
 .then(()=>app.listen(process.env.PORT||8080))
