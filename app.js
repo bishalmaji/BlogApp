875d50d2a3e97c3f92a40cfac3c4d9193b1a3e66
bishalmaji <bishalmaji60.lj@gmail.com>
2022-10-20 16:55:28 +0530

    Home Page Commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Welcome to Blog App API Freatures-Signin, Signup, Add blog,Get all blogs, Get user blogs, Update a blog, Delete a blog</h1>M');
+    res.send('<!DOCTYPE html><html lang="en"><head><title>Blog Api</title></head><body><h1>Welcome to Blog Api</h1><br><h2>Freatures of this Api :</h2><ol><li>Get All Blogs -(GET) https://blog-app8102.herokuapp.com/api/blog</li> <li>Get User Blogs-(GET) https://blog-app8102.herokuapp.com/api/blog/user/:id</li><li>Get User List -(GET) https://blog-app8102.herokuapp.com/api/user </li><li>Signin-(POST) https://blog-app8102.herokuapp.com/api/user/login, End points- email,password</li><li>Signup-(POST) https://blog-app8102.herokuapp.com/api/user/signup, End points- name,email,password</li><li>Add a Blog-(POST) https://blog-app8102.herokuapp.com/api/blog/add, End points- title,description,image,user</li><li>Update a Blog-(PUT) https://blog-app8102.herokuapp.com/api/blog/update/:id, End points-title,description</li><li>Delete a Blog-(DELETE) https://blog-app8102.herokuapp.com/api/blog/:id</li></ol></body></html>');
     res.end();
 });
 mongoose.connect(process.env.DATABASE
